6d7a71044c5cd1ec01bbf2bb9bf57b8d8232b7c0
PrasadK05 <kardeprasad1996@gmail.com>
2022-10-14 17:58:02 +0530

    added portfolio

--- src/App.js
+++ src/App.js
@@ -5,18 +5,9 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Greetings</h1>
+       <h1>Hii I Am Prasad Karde</h1>
+       <h3>Full Stack Web Developer</h3>       
       </header>
     </div>
   );
